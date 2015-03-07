770de41e39cdf58b4f9af4931d83e5da024727f4
Pavlin Gergov <pgergov92@abv.bg>
2015-03-07 10:47:24 +0200

    Adding info to magic square

--- week5/Magic-Square/magic.py
+++ week5/Magic-Square/magic.py
@@ -3,18 +3,24 @@ def magic_square(square):
     forward_diagonal = 0
     backward_diagonal = 0
     is_true = True
+
+#Adding every row sum to sum_all
     
     for row in square:
         s = 0
         for element in row:
             s += element
         sum_all.append(s)
+
+#Adding every column sum to sum_all
         
     for i in range(0, len(square)):
         s = 0
         for row in square:
             s += row[i]
         sum_all.append(s)
+
+#Adding the two diagonals to sum_all
         
     for i in range(0, len(square)):
         forward_diagonal += square[i][i]
@@ -23,6 +29,8 @@ def magic_square(square):
     for i in range(len(square) - 1, -1, -1):
         backward_diagonal += square[i][i]
     sum_all.append(backward_diagonal)
+
+#Checking if every element in sum_all is equal
 
     for i in range(0, len(sum_all)):
         if i + 1 < len(sum_all):
@@ -33,3 +41,4 @@ def magic_square(square):
 
 print(magic_square([ [23, 28, 21], [22, 24, 26], [27, 20, 25] ]))
 print(magic_square([ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]))
+print(magic_square([]))
