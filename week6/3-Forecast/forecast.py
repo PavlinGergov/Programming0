374c96062cec9aa899a98e13271dfb203bce850e
Pavlin Gergov <pgergov92@abv.bg>
2015-03-12 02:41:37 +0200

    Added forecast

--- week6/3-Forecast/forecast.py
+++ week6/3-Forecast/forecast.py
@@ -1,37 +1,16 @@
 def forecast(info):
-    result = {
-                "rain": 0,
-                "snow": 0,
-                "sunshine": 0,
-             }
-
-    for day in info:
-        result[day] += 1
-
-    days = []
-    weather = []
-    for item in result:
-        weather.append(item)
-        days.append(result[item])
-
-    equal = 0
-    current = -1
-    
-    for element in result:
-        if current != -1:
-            if current == result[element]:
-                equal += 1
-            else:
-                the_day = element
-        else:
-            current = result[element]
-
-    if equal == 1:
-        return the_day
-    elif equal == 2:
-        return info[len(info) - 1]
-    else:
-        return weather[days.index(max(days))]
+    weather = ["sunshine", "rain", "snow"]
+    days = [0,0,0]
+    for element in info:
+        if element == "sunshine": days[0] += 1
+        elif element == "rain": days[1] += 1
+        elif element == "snow": days[2] += 1
+    if days[0] == max(days) and days[0] == days[1]:
+        if days[0] == days[2]: return info[len(info) - 1]
+        else: return weather[2]
+    elif days[0] == max(days) and days[0] == days[2]: return weather[1]
+    elif days[1] == max(days) and days[1] == days[2]: return weather[0]
+    else: return weather[days.index(max(days))]
 
 print(forecast(["snow", "snow", "rain", "sunshine"]))
 print(forecast(["rain", "rain", "snow", "snow", "sunshine", "sunshine"]))
