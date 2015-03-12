90079addf99a0686f09edc153ed209af589cd466
Pavlin Gergov <pgergov92@abv.bg>
2015-03-12 03:03:00 +0200

    Added Tic-Tac-Toe

--- week6/4-Print-Tic-Tac-Toe/tic.py
+++ week6/4-Print-Tic-Tac-Toe/tic.py
@@ -7,20 +7,13 @@ def join(delimiter, items):
     return result
 
 def board_to_string(board):
-    result = []
-    res = []
-    for item in board:
-        current = ""
-        for element in item:
-            current += element
-        result.append(join(" | ", current))
-    for item in result:
-        print(item)
+    result = "| "
+    for i in range(len(board)):
+        board[i] = " | ".join(board[i])
+    result += join(" |\n| ", board)
+    result += " |"
     return result
 
-
-
-
 print(board_to_string([ ["X", "O", "#"],
-          ["X", "X", "X"],
-          ["#", "#", "#"] ]))
+                        ["X", "X", "X"],
+                        ["#", "#", "#"] ]))
