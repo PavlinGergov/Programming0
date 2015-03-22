7d77f108a6e21f75734a5d0e48ba05c1afb44ef5
Pavlin Gergov <pgergov92@abv.bg>
2015-03-22 14:16:50 +0200

    Fixed second diagonal in magic_square

--- week5/Magic-Square/magic.py
+++ week5/Magic-Square/magic.py
@@ -1,44 +1,24 @@
-def magic_square(square):
-    sum_all = []
-    forward_diagonal = 0
-    backward_diagonal = 0
-    is_true = True
+def magic_square(matrix):
+    s = []
 
-#Adding every row sum to sum_all
-    
-    for row in square:
-        s = 0
-        for element in row:
-            s += element
-        sum_all.append(s)
+# Sum of rows:
+    for row in matrix:
+        s.append(sum(row))
+# Sum of columns:
+    for i in range(0, len(matrix)):
+        s.append(sum([row[i] for row in matrix]))
+# Sum of diagonals:
+    s.append(sum([matrix[i][i] for i in range(len(matrix))]))
 
-#Adding every column sum to sum_all
-        
-    for i in range(0, len(square)):
-        s = 0
-        for row in square:
-            s += row[i]
-        sum_all.append(s)
+    i = 0
+    result = 0
+    for j in range(len(matrix) - 1, -1, -1):
+        result += matrix[i][j]
+        i += 1
+    s.append(result)
 
-#Adding the two diagonals to sum_all
-        
-    for i in range(0, len(square)):
-        forward_diagonal += square[i][i]
-    sum_all.append(forward_diagonal)
-    
-    for i in range(len(square) - 1, -1, -1):
-        backward_diagonal += square[i][i]
-    sum_all.append(backward_diagonal)
+    return all([s[0] == s[i] for i in range(len(s))])
 
-#Checking if every element in sum_all is equal
-
-    for i in range(0, len(sum_all)):
-        if i + 1 < len(sum_all):
-            if sum_all[i] != sum_all[i+1]:
-                is_true = False
-                break
-    return is_true
-
-print(magic_square([ [23, 28, 21], [22, 24, 26], [27, 20, 25] ]))
-print(magic_square([ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]))
+print(magic_square([[23, 28, 21], [22, 24, 26], [27, 20, 25]]))
+print(magic_square([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
 print(magic_square([]))
